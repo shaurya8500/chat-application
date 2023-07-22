c52fe82a2d6a1f11e35414b5b725e9e5455c17c6
shaurya8500 <201b331@juetguna.in>
2023-07-22 13:04:38 +0530

    files adding

---
+++ src/components/Input.jsx
@@ -0,0 +1,108 @@
+import React, { useContext, useState } from "react";
+import Img from "../img/img.png";
+import Attach from "../img/attach.png";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
+const Input = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (!data.chatId) {
+      console.log("No valid chatId found.");
+      return;
+    }
+
+    if (img) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      try {
+        const snapshot = await uploadTask;
+        const downloadURL = await getDownloadURL(snapshot.ref);
+
+        await updateDoc(doc(db, "chats", data.chatId), {
+          messages: arrayUnion({
+            id: uuid(),
+            text,
+            senderId: currentUser.uid,
+            date: Timestamp.now(),
+            img: downloadURL,
+          }),
+        });
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    const lastMessageData = {
+      text,
+      date: serverTimestamp(),
+    };
+
+    try {
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [data.chatId]: lastMessageData,
+      });
+
+      await updateDoc(doc(db, "userChats", data.user.uid), {
+        [data.chatId]: lastMessageData,
+      });
+
+      setText("");
+      setImg(null);
+    } catch (error) {
+      console.error("Error updating userChats:", error);
+    }
+  };
+
+  return (
+    <div className="input">
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button onClick={handleSend}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
